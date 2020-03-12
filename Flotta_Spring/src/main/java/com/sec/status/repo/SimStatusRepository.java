0916c388a97cbca8491ec6a0368235094822fbc2
czakopeter <czakopeter@freemail.hu>
2020-03-12 19:19:17 +0100

    c

---
+++ Flotta_Spring/src/main/java/com/sec/status/repo/SimStatusRepository.java
@@ -0,0 +1,17 @@
+package com.sec.status.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sec.entity.Sim;
+import com.sec.status.SimStatus;
+
+public interface SimStatusRepository extends CrudRepository<SimStatus, Long> {
+
+  List<SimStatus> findAllBySim(Sim sim);
+
+  SimStatus findFirstBySimOrderByConnectDesc(Sim sim);
+
+  void deleteAllBySim(Sim sim);
+}
---
+++ Flotta_Spring/src/main/java/com/sec/status/service/SimStatusService.java
@@ -0,0 +1,58 @@
+package com.sec.status.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sec.status.SimStatus;
+import com.sec.status.repo.SimStatusRepository;
+import com.sec.entity.Sim;
+import com.sec.enums.SimStatusEnum;
+
+@Service
+public class SimStatusService {
+
+  private SimStatusRepository simStatusRepository;
+
+  @Autowired
+  public void setSimStatusRepository(SimStatusRepository simStatusRepository) {
+    this.simStatusRepository = simStatusRepository;
+  }
+
+  public SimStatus findById(long id) {
+    return simStatusRepository.findOne(id);
+  }
+
+  public List<SimStatus> findAllBySim(Sim sim) {
+    return simStatusRepository.findAllBySim(sim);
+  }
+
+  public boolean save(Sim sim, String status, LocalDate date) {
+    try {
+      simStatusRepository.save(new SimStatus(SimStatusEnum.valueOf(status), sim, date));
+    } catch (Exception e) {
+      return false;
+    }
+    return true;
+  }
+  
+  public void deleteLastStatus(Sim sim) {
+    SimStatus status = simStatusRepository.findFirstBySimOrderByConnectDesc(sim);
+    if(status != null && !status.isFree()) {
+      simStatusRepository.delete(status);
+    }
+  }
+  
+  public void deleteAllStatus(Sim sim) {
+    simStatusRepository.deleteAllBySim(sim);
+  }
+
+  public void modifyLastStatus(Sim sim, String imeiChangeReason) {
+    SimStatus ss = simStatusRepository.findFirstBySimOrderByConnectDesc(sim);
+    ss.setStatus(SimStatusEnum.valueOf(imeiChangeReason));
+    simStatusRepository.save(ss);
+  }
+  
+}
